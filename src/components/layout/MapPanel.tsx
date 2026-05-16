7be34d8532fd09a201075e9648f2ad0bc23d021f
Vincent <aoc7328@gmail.com>
2026-05-16 12:26:31 +0800

    ui: remove map legend from MapPanel

--- src/components/layout/MapPanel.tsx
+++ src/components/layout/MapPanel.tsx
@@ -4,20 +4,6 @@ export default function MapPanel() {
   return (
     <section className="map-panel">
       <MapView />
-      <div className="map-legend">
-        <div className="legend-item">
-          <span className="legend-dot hotel" />
-          <span>飯店</span>
-        </div>
-        <div className="legend-item">
-          <span className="legend-dot added" />
-          <span>已加入</span>
-        </div>
-        <div className="legend-item">
-          <span className="legend-dot search" />
-          <span>搜尋結果</span>
-        </div>
-      </div>
     </section>
   );
 }
